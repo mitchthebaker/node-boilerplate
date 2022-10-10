0fb5f58a73cf8a330561fba7e4725334ddbaee34
Mitchel Baker <mitchthebaker@Mitchels-MacBook-Pro.local>
2022-10-10 12:47:00 -0700

    client can submit messages to api-gateway

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -3,17 +3,42 @@ import axios from 'axios';
 import './App.css';
 
 const App = () => {
+  const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API_GATEWAY_URI}/messages`).then(result => {
       const data = result.data;
       setMessages(data);
+    })
+    .catch(err => {
+      console.error(`Error when getting messages, ${err}`);
     });
   }, []);
 
+  const handleChange = (e) => setMessage(e.target.value);
+
+  const handleSubmit = () => {
+    console.log(message);
+    axios.post(`${process.env.REACT_APP_API_GATEWAY_URI}/messages`, {
+      message: message
+    }).then(result => {
+      const data = result.data;
+      
+      // do something with the response sent from server
+    })
+    .catch(err => {
+      console.error(`Error when posting a message, ${err}`);
+    });
+  };
+
   return (
     <div className="App">
+      <div>
+        <label> Send a message </label>
+        <input type="text" onChange={e => handleChange(e)} />
+        <button onClick={handleSubmit}> Submit </button>
+      </div>
       {messages && messages.map((item, idx) => (
         <div key={idx}>
           { item.message }
