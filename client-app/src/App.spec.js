71700069c4c187e033bb6b62b69f2f9b37833f3c
Mitchel Baker <mitchel@hashy.so>
2024-07-16 11:42:16 -0700

    create initial unit testing workflow with playwright; add initial unit test

---
+++ client-app/src/App.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import App from './App';
+
+test.use({ viewport: { width: 500, height: 500 } });
+
+test('Verify components exist in App.js', async ({ mount }) => {
+  const component = await mount(<App />);
+  await expect(component).toContainText('Send a message');
+  await expect(component).toContainText('save to reload');
+});
+
+// goto url
