afd7207bead15046275e00e487ec1f490f45756d
Mitchel Baker <32985849+mitchthebaker@users.noreply.github.com>
2024-07-28 21:25:40 -0700

    Buildout docker workflow; add e2e-test service (#3)
    * test if docker buildx/dockerhub can be removed
    
    * break out .env creation into action
    
    * break out .env creation into action
    
    * break out .env creation into action
    
    * break out .env creation into action
    
    * create dockerhub-deploy action to build/push images to dockerhub
    
    * create dockerhub-deploy action to build/push images to dockerhub
    
    * create dockerhub-deploy action to build/push images to dockerhub
    
    * create dockerhub-deploy action to build/push images to dockerhub
    
    * create dockerhub-deploy action to build/push images to dockerhub
    
    * update docker-compose to use dockerhub image
    
    * may not need separate action to push images; just use docker-compose push; lets see if it works
    
    * make some final changes to docker container workflow; some small changes to api-gateway, then add e2e test service
    
    ---------
    
    Co-authored-by: Mitchel Baker <mitchel@hashy.so>

---
+++ e2e-tests/playwright.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  testDir: './tests',
+  timeout: 10 * 1000,
+  retries: 0,
+  use: {
+    //baseUrl: "http://127.0.0.1:3000",
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'Desktop Chromium',
+      use: { browserName: 'chromium' },
+    },
+    //{
+    //  name: 'Desktop Firefox',
+    //  use: { browserName: 'firefox' },
+    //},
+    //{
+    //  name: 'Desktop WebKit',
+    //  use: { browserName: 'webkit' },
+    //},
+  ],
+    // Configure web server for client-app
+    webServer: {
+      command: 'npm start',
+      url: 'http://127.0.0.1:3000',
+      reuseExistingServer: !process.env.CI,
+    },
+    globalSetup: require.resolve('./setup.js'),
+};
