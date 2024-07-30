0824a6281965c1c01189dbd9e5b9e4a19e059316
Mitchel Baker <mitchel@hashy.so>
2024-07-29 23:34:06 -0700

    comment out playwright unit test files

--- client-app/src/App.spec.js
+++ client-app/src/App.spec.js
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/experimental-ct-react';
-import App from './App';
-
-test.use({ viewport: { width: 500, height: 500 } });
-
-test('Verify components exist in App.js', async ({ mount }) => {
-  const component = await mount(<App />);
-  await expect(component).toContainText('Send a message');
-  await expect(component).toContainText('Submit');
-});
+//import { test, expect } from '@playwright/experimental-ct-react';
+//import App from './App';
+//
+//test.use({ viewport: { width: 500, height: 500 } });
+//
+//test('Verify components exist in App.js', async ({ mount }) => {
+//  const component = await mount(<App />);
+//  await expect(component).toContainText('Send a message');
+//  await expect(component).toContainText('Submit');
+//});
