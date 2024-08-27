103bd92ed542cdbbe5222c890c3731ee38a28b5c
Mitchel Baker <mitchthebaker@protonmail.com>
2024-08-27 23:28:45 +0000

    modify deploy.sh to use prod docker-compose

--- api-gateway/scripts/deploy.sh
+++ api-gateway/scripts/deploy.sh
@@ -3,4 +3,4 @@
 cd ../../
 docker compose down 
 docker compose pull
-docker compose -f docker-compose.yml -f docker-compose.local.yml up -d api-gateway client-app server-db pgadmin
+docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
