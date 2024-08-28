8cea903f07dd3fba34bc0060a71305057f03c929
Mitchel Baker <mitchel@hashy.so>
2024-08-27 17:29:47 -0700

    update deploy.sh script

--- api-gateway/scripts/deploy.sh
+++ api-gateway/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd ../../
 docker compose down 
 docker compose pull
+cd ../../../
+ls
 docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
