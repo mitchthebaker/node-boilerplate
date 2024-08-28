a8abc7e562087026f060df0dd36cf5d55ca5c4ad
Mitchel Baker <mitchthebaker@protonmail.com>
2024-08-28 00:49:04 +0000

    fix setupRoutes.js

--- api-gateway/src/server/setupRoutes.js
+++ api-gateway/src/server/setupRoutes.js
@@ -63,7 +63,7 @@ const setupRoutes = (app) => {
 
       if (req.body.ref === 'refs/heads/main') {
 	exec("pwd");      
-        exec('sh ./api-gateway/scripts/deploy.sh', (error, stdout, stderr) => {
+        exec('sh ./scripts/deploy.sh', (error, stdout, stderr) => {
             if (error) {
                 console.error(`exec error: ${error}`);
                 return res.status(500).send(error);
