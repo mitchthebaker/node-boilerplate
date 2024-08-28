f49fcba7b111292425b7a144e1d73a9ac0db9003
Mitchel Baker <mitchthebaker@protonmail.com>
2024-08-28 00:54:30 +0000

    update deploy.sh

--- api-gateway/scripts/deploy.sh
+++ api-gateway/scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 cd ../../
 ls
-docker compose down 
-docker compose pull
-docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
+/usr/bin/docker compose down 
+/usr/bin/docker compose pull
+/usr/bin/docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
