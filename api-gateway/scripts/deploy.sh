3469f4d1a635b45c17501b7b8b5527b150fca4e6
Mitchel Baker <mitchthebaker@protonmail.com>
2024-08-28 01:13:35 +0000

    try adding docker inside dontainer

--- api-gateway/scripts/deploy.sh
+++ api-gateway/scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 cd ../../
 ls
-/usr/bin/docker compose down 
-/usr/bin/docker compose pull
-/usr/bin/docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
+docker compose down 
+docker compose pull
+docker compose -f docker-compose.yml -f docker-compose.production.yml up -d api-gateway client-app server-db pgadmin
